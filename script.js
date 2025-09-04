1a7c0a177f00d4bddb30f8499bf924e00e57c02a
majlismsah <majelisalasmaulhusna@gmail.com>
2025-09-04 12:35:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ====== KONFIGURASI ======
 
 // 1) URL Web App Google Apps Script
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxmcqN01icnk1q2I8tM9lkQhlD3tIGQNtKBH94oxbcG2M3kbQPZi7BjnQo_WdCoEjs/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzLGbF35x5olhvD3cNqvd4STKQMN-UUOBVUzT6c9N_7hCz_XH9YraaFrUActP5u8eZ0/exec";
 
 // 2) Nomor WA Admin Pusat
 const ADMIN_PUSAT = "62816787977";
